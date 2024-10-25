4ff930332c9eeb9757c0072e7b47360006e50552
Github MMI3 <github@mmi-troyes.fr>
2024-10-25 16:29:14 +0000

    feat: user features

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,6 +9,8 @@ use DateTimeImmutable;
 use App\Entity\Actor;
 use App\Entity\Category;
 use App\Entity\Movie;
+use App\Entity\User;
+use DateTime;
 
 class AppFixtures extends Fixture
 {
@@ -40,12 +42,30 @@ class AppFixtures extends Fixture
 
         $faker->addProvider(new \Xylis\FakerCinema\Provider\Movie($faker));
         $movies = $faker->movies(100);
-        // $directors = $faker->directors();
+
+        $insertedGenre = [];
+        $genres = $faker->movieGenres($count = 21, $duplicates = false);
+
+        foreach ($genres as $item) {
+
+            if (!array_key_exists($item, $insertedGenre)) {
+                $genre = new Category();
+                $genre->setTitle($item);
+                $genre->setCreatedAt(new DateTimeImmutable());
+
+                $manager->persist($genre);
+
+                $insertedGenre[$item] = $genre;
+            }
+        }
+
+        $directors = $faker->directors($gender = null, $count = 140, $duplicates = false);
         foreach ($movies as $item) {
             $movie = new Movie();
             $movie->setTitle($item);
-            $movie->setDirector("test");
             $movie->setEntries(rand(1000, 1500000));
+            $date = $faker->dateTime();
+            $movie->setReleaseDate($date->format('d/m/Y'));
             $movie->setCreatedAt(new DateTimeImmutable());
 
             shuffle($createdActors);
@@ -53,49 +73,31 @@ class AppFixtures extends Fixture
             foreach ($createdActorsSliced as $actor) {
                 $movie->addActor($actor);
             }
-            $manager->persist($movie);
 
-            $insertedGenre = [];
-            
-            $genres = $faker->movieGenres();
-
-            
-            foreach ($genres as $item) {
-                // var_dump($item, $insertedGenre);
-            
-                if (!array_key_exists($item, $insertedGenre)) {
-                    $genre = new Category();
-                    $genre->setTitle($item);
-                    $genre->setCreatedAt(new DateTimeImmutable());
-            
-                    $manager->persist($genre);
-            
-                    $insertedGenre[$item] = $genre;
-                } 
-                // else {
-                //     $insertedGenre[$item] = $genre;
-                //     var_dump("Genre déjà inséré : " . $item); // Message si le genre existe déjà
-                // }
+            shuffle($insertedGenre);
+            $insertedGenreSliced = array_slice($insertedGenre, 0, 2);
+            foreach ($insertedGenreSliced as $categorie) {
+                $movie->addCategory($categorie);
             }
-            
 
-            // $genres = $item->movieGenres();
-            // foreach ($genres as $item) {
-            //     if (!array_key_exists($item, $insertedGenre)) {
-            //         $genre = new Category();
+            shuffle($directors);
+            $createdDirectorsSliced = array_slice($directors, 0, 1);
+            foreach ($createdDirectorsSliced as $director) {
+                // var_dump($director);
+                $movie->setDirector($director);
+            }
 
-            //         $genre->setTitle($item);
-            //         $this->em->persist($genre);
-
-            //         $inserted[$item] = $genre;
-
-
-            //     } 
-            //     // else {
-            //     //     $inserted[$item] = $genre;
-            //     // }
-            // }
+            $manager->persist($movie);
         }
+
+        $user = new User();
+
+        $user->setEmail('admin@admin.com');
+        // password hashed is -> 'admin'
+        $user->setPassword('$2y$13$1pFzf2EDRflvb9ildEfi1OoCtxK.4dD/hvDT2LC6nDJlhXrFjIt2i');
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($user);
 
         $manager->flush();
         return true;
